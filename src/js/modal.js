d0814ed4d6632a38c544cc3a542ebbb29a3560f9
João oliveira <joaopedrooliveira6704@gmail.com>
2025-05-12 10:58:09 +0100

    feat: Added register form
    Added register form (only ui) and some basic javascript (to be improved).

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,14 @@
-const openModalBtn = document.getElementById("openModalBtn");
+const openLoginModalBtn = document.getElementById("openLoginModalBtn");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const loginModal = document.getElementById("loginModal");
+const registerModal = document.getElementById("registerModal");
 const modalContent = document.getElementById("modalContent");
+const modalRegisterContent = document.getElementById("modalRegisterContent");
+const openRegisterBtn = document.getElementById("signUpText");
 
-openModalBtn.addEventListener("click", () => {
+/* Login */
+
+openLoginModalBtn.addEventListener("click", () => {
   loginModal.classList.remove("hidden");
   setTimeout(() => {
     modalContent.classList.remove("opacity-0", "scale-95");
@@ -11,18 +16,43 @@ openModalBtn.addEventListener("click", () => {
   }, 10);
 });
 
+/* Register */
+
+openRegisterBtn.addEventListener("click", () => {
+  loginModal.classList.add("hidden");
+  registerModal.classList.remove("hidden");
+  setTimeout(() => {
+    modalContent.classList.remove("opacity-100", "scale-100");
+    modalContent.classList.add("opacity-0", "scale-95");
+    modalRegisterContent.classList.remove("opacity-0", "scale-95");
+    modalRegisterContent.classList.add("opacity-100", "scale-100");
+  }, 10);
+});
+
+/* Both */
+
 closeModalBtn.addEventListener("click", closeModal);
 
 window.addEventListener("click", (e) => {
   if (e.target === loginModal) {
-    closeModal();
+    closeLoginModal();
+  } else if (e.target === registerModal) {
+    closeRegisterModal();
   }
 });
 
-function closeModal() {
+function closeLoginModal() {
   modalContent.classList.remove("opacity-100", "scale-100");
   modalContent.classList.add("opacity-0", "scale-95");
   setTimeout(() => {
     loginModal.classList.add("hidden");
+  }, 300);
+}
+
+function closeRegisterModal() {
+  modalRegisterContent.classList.remove("opacity-100", "scale-100");
+  modalRegisterContent.classList.add("opacity-0", "scale-95");
+  setTimeout(() => {
+    registerModal.classList.add("hidden");
   }, 300);
 }
