490389488dc780536ac78982854a7b7a565e26ea
João Oliveira <joaopedrooliveira6704@gmail.com>
2025-05-14 09:59:40 +0100

    feat: Register full action
    Full register action added. Writing on json.

--- app.js
+++ app.js
@@ -10,28 +10,28 @@ app.use(express.static("src"));
 // Para aceitar JSON vindo do cliente (fetch POST, etc)
 app.use(express.json());
 
-// Rota de teste para registo
+const userPath = path.join(__dirname, "src/json/users.json");
+
+// Rota POST para registar novo usuário
 app.post("/api/register", (req, res) => {
-  console.log("Dados recebidos:", req.body);
-  res.json({ message: "Registo recebido!" });
+  const newUser = req.body;
+  updateUsers(newUser);
+  res.json({ message: "Utilizador registado com sucesso!" });
 });
 
+function getUsers() {
+  const data = fs.readFileSync(userPath);
+  return JSON.parse(data);
+}
+
+function updateUsers(newUser) {
+  const users = getUsers();
+  users.push(newUser);
+  fs.writeFileSync(userPath, JSON.stringify(users, null, 2));
+}
+
 app.get("/api/users", (req, res) => {
-  const filePath = path.join(__dirname, "src/json/users.json");
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return res.status(500).json({ error: "Failed to load users" });
-    }
-
-    try {
-      res.json(JSON.parse(data));
-    } catch (parseErr) {
-      console.error("Error parsing JSON:", parseErr);
-      res.status(500).json({ error: "Invalid JSON format" });
-    }
-  });
+  res.json(getUsers());
 });
 
 app.listen(port, () => {
