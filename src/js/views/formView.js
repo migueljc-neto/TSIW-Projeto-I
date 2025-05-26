9e76530ebd4309967ae8af58cb2583b88b0ed126
migueljcneto <40240358@esmad.ipp.pt>
2025-05-26 12:34:52 +0100

    fix: Tweaks to tourism type options

--- src/js/views/formView.js
+++ src/js/views/formView.js
@@ -15,7 +15,7 @@ window.addEventListener("load", (event) => {
       `<div
               class="px-2 py-1 flex items-center gap-2 w-full last:rounded-b-md first:rounded-t-md hover:bg-gray-100"
             >
-              <h1>${tourismType.name}</h1>
+            <button class="cursor-pointer">${tourismType.name}</button>
             </div>`
     );
   });
