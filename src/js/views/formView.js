479179e0734505eb3f9e1ba93b9b07cb4926f4d1
migueljcneto <40240358@esmad.ipp.pt>
2025-05-26 14:07:55 +0100

    fix: Tweaks to passenger number selector

--- src/js/views/formView.js
+++ src/js/views/formView.js
@@ -13,9 +13,9 @@ window.addEventListener("load", (event) => {
     tourismFormSection.insertAdjacentHTML(
       "beforeend",
       `<div
-              class="px-2 py-1 flex items-center gap-2 w-full last:rounded-b-md first:rounded-t-md hover:bg-gray-100"
+              class="px-2 py-1 flex items-center gap-2 w-full last:rounded-b-md first:rounded-t-md hover:bg-gray-100 cursor-pointer"
             >
-            <button class="cursor-pointer">${tourismType.name}</button>
+            <button>${tourismType.name}</button>
             </div>`
     );
   });
