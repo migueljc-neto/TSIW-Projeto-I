4dd4b9bdb313b220a8b3cf31059f5e1aef93ec13
João Oliveira <40240391@esmad.ipp.pt>
2025-05-27 22:29:50 +0100

    refactor: Merge login and register view.
    Merged login and register view to AuthView.

--- js/views/AuthView.js
+++ js/views/AuthView.js
@@ -2,6 +2,36 @@ import * as User from "../models/userModel.js";
 
 User.init();
 
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  try {
+    User.login(
+      document.getElementById("emailLogin").value,
+      document.getElementById("passwordLogin").value,
+      document.getElementById("keepSigned").checked
+    );
+
+    const loginModal = document.getElementById("loginModal");
+    loginModal.classList.add("hidden");
+  } catch (e) {
+    displayMessage(e.message);
+  }
+});
+
+function displayMessage(message) {
+  const oldMessage = loginForm.querySelector(".text-red-500");
+  if (oldMessage) {
+    oldMessage.remove();
+  }
+
+  loginForm.insertAdjacentHTML(
+    "beforeend",
+    `<p class='text-red-500 mt-2'>${message}</p>`
+  );
+}
+
 const registerForm = document.getElementById("registerForm");
 
 registerForm.addEventListener("submit", (event) => {
