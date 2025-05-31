2c6518aa1fb44ee64b2fac204a13d89631cb6269
João Oliveira <40240391@esmad.ipp.pt>
2025-05-31 20:57:31 +0100

    feat: Added swiper. Fix dimensions
    Added desktop swiper for packs and fixed dimensions

--- js/views/MainView.js
+++ js/views/MainView.js
@@ -2,31 +2,28 @@ import * as Trips from "../models/TripModel.js";
 
 Trips.init();
 
-window.addEventListener("load", async () => {
+window.addEventListener("load", () => {
   const packs = Trips.getAllPacks();
   const swiperPacks = document.getElementById("swiperPacks");
-
+  const swiperDesktop = document.getElementById("swiperDesktopWrapper");
   const apiKey = "NpYuyyJzclnrvUUkVK1ISyi2FGnrw4p9sNg9CCODQGsiFc0nWvuUJJMN";
 
-  const fetchSlides = packs.map(async (pack) => {
+  const fetchSlides = packs.map((pack) => {
     const query = `${pack.name} city`;
 
-    try {
-      const response = await fetch(
-        `https://api.pexels.com/v1/search?query=${query}&per_page=1`,
-        {
-          headers: { Authorization: apiKey },
-        }
-      );
-      const data = await response.json();
-      const image = data.photos[0]?.src.medium || "fallback.jpg";
+    return fetch(`https://api.pexels.com/v1/search?query=${query}&per_page=1`, {
+      headers: { Authorization: apiKey },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const image = data.photos[0]?.src.medium || "fallback.jpg";
 
-      swiperPacks.insertAdjacentHTML(
-        "beforeend",
-        `<div class="swiper-slide relative cursor-drag">
+        swiperPacks.insertAdjacentHTML(
+          "beforeend",
+          `<div class="swiper-slide relative cursor-drag">
             <img src="${image}" alt="${
-          pack.name
-        }" class="w-full h-auto rounded-t-lg" />
+            pack.name
+          }" class="w-full h-auto rounded-t-lg" />
             <div class="absolute backdrop-blur-sm bottom-0 left-0 p-5 w-full h-30 bg-white bg-opacity-80 text-black p-2">
                 <div class="flex gap-6 items-center font-space font-light mb-3">
                     <p class="text-lg">${pack.name}</p>
@@ -36,19 +33,70 @@ window.addEventListener("load", async () => {
                 </div>
                 <div class="color-primary font-light">${pack.price}€</div>
             </div>
-        </div>`
-      );
-    } catch (error) {
-      console.error("Image fetch failed:", error);
-    }
+          </div>`
+        );
+        swiperDesktop.insertAdjacentHTML(
+          "beforeend",
+          `<div class="swiper-slide rounded-lg w-[320px] h-[420px] flex flex-col overflow-hidden cursor-drag">
+            <img src="${image}" alt="${
+            pack.name
+          }" class="w-full h-[220px] object-cover rounded-t-lg" />
+            <div class="bg-white bg-opacity-90 p-4 color-primary flex flex-col justify-between h-full w-full backdrop-blur-md">
+
+              <div>
+                <div class="flex gap-4 mb-5 items-center justify-between">
+                  <div>
+                    <p class="font-space font-light text-lg">${pack.name}</p>
+                    <p class="font-space font-light text-sm">${formatDateToLabel(
+                      pack.startDate
+                    )} - ${formatDateToLabel(pack.endDate)}</p>
+                  </div>
+                  <div class="text-sm font-light">
+                    <ul>
+                      <li>location</li>
+                      <li>location</li>
+                      <li>location</li>
+                      <li>location</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+
+              <div class="flex justify-between items-center text-primary font-light">
+                <span>${pack.price}€</span>
+                <button class="btn-std cursor-pointer">Ver Pack</button>
+              </div>
+
+            </div>
+          </div>`
+        );
+      });
   });
 
-  await Promise.all(fetchSlides);
-
-  // Now initialize Swiper
-  new Swiper(".mySwiper", {
-    effect: "cards",
-    grabCursor: true,
+  Promise.all(fetchSlides).then(() => {
+    new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+    });
+    new Swiper(".packsDesktop", {
+      effect: "coverflow",
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
   });
 });
 
