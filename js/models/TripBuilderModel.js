543b802d8863ffc275ce0bc638d4d69a81ee6e65
migueljcneto <40240358@esmad.ipp.pt>
2025-06-01 17:01:06 +0100

    feat: Trip builder creation
    Implementing map and draggable list items

---
+++ js/models/TripBuilderModel.js
@@ -0,0 +1,12 @@
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+new Sortable(document.getElementById("destinationSortableList"), {
+  animation: 150,
+  ghostClass: "bg-blue-100", // Optional: adds class to the dragged item
+});
