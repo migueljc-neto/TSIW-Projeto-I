460ca0c6b3e17f8b6eb08c6c50b7fd4bcaef8355
migueljcneto <40240358@esmad.ipp.pt>
2025-06-01 21:58:33 +0100

    feat: Developed  draggable items to add to the destination list

--- js/models/TripBuilderModel.js
+++ js/models/TripBuilderModel.js
@@ -6,7 +6,35 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
 
+/* Sortabel Lists */
 new Sortable(document.getElementById("destinationSortableList"), {
+  group: {
+    name: "destinationListGroup",
+  },
+
   animation: 150,
-  ghostClass: "bg-blue-100", // Optional: adds class to the dragged item
+
+  onAdd: function (cell) {
+    console.log(cell.item);
+    item = cell.item;
+
+    console.log(item.innerHtml);
+
+    /* const item = cell.item; */
+    /* item.classlist.remove("bg-white", "p-4", "rounded");
+
+    item.classlist.add("") */
+    /* 
+    class="pr-10 pl-5 bg-[#39578A] focus:bg-[#6C6EA0] text-white h-20 w-full flex items-center justify-between rounded-lg" */
+  },
+});
+
+new Sortable(document.getElementById("destinationSortableListView"), {
+  group: {
+    name: "destinationListGroup",
+    pull: "clone",
+    put: "false",
+    sort: "false",
+  },
+  animation: 150,
 });
--- js/views/TripBuilderView.js
+++ js/views/TripBuilderView.js
@@ -1,1 +1,29 @@
 console.log("hello");
+
+const mapViewBtn = document.getElementById("mapViewBtn");
+const listViewBtn = document.getElementById("listViewBtn");
+
+const mapCell = document.getElementById("map");
+const listViewCell = document.getElementById("mapListView");
+
+let mapView = true;
+
+mapViewBtn.addEventListener("click", function () {
+  if (!mapView) {
+    mapCell.classList.toggle("hidden");
+    listViewCell.classList.toggle("hidden");
+    mapViewBtn.classList.toggle("border-b-0");
+    listViewBtn.classList.toggle("border-b-0");
+    mapView = true;
+  }
+});
+
+listViewBtn.addEventListener("click", function () {
+  if (mapView) {
+    mapCell.classList.toggle("hidden");
+    listViewCell.classList.toggle("hidden");
+    mapViewBtn.classList.toggle("border-b-0");
+    listViewBtn.classList.toggle("border-b-0");
+    mapView = false;
+  }
+});
